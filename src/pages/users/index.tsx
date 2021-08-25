d1b68738594f0805f0730bd8934fe34360816280
Gustavo Albunio <front.end.grupoacert@gmail.com>
2021-08-25 10:34:56 -0300

    criaçao e listagem de usuario finalizada

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,19 +13,46 @@ import {
   Td,
   Text,
   useBreakpointValue,
+  Spinner,
 } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
 import Link from 'next/link';
+import { useState } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
+import { getUsers, useUsers } from '../../services/hooks/useUsers';
+import { queryClient } from '../../services/queryClient';
 
-export default function UserList() {
+export default function UserList({ users }) {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(
+      ['user', userId],
+      async () => {
+        const response = await api.get(`users/${userId}`);
+
+        return response.data;
+      },
+      {
+        staleTime: 1000 * 60 * 10, // 10 minutes
+      }
+    );
+  }
+
+  console.log(data);
 
   return (
     <Box>
@@ -38,6 +65,9 @@ export default function UserList() {
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size='sm' color='gray.500' ml='4' />
+              )}
             </Heading>
 
             <Link href='/users/create' passHref>
@@ -53,114 +83,87 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && (
-                  <>
-                    <Th>Data de cadastro</Th>
-                    <Th width='8'></Th>
-                  </>
-                )}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Gustavo Albunio</Text>
-                    <Text fontSize='sm' color='gray.300'>
-                      gustavo.albunio@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>01 de Junho, 2021</Td>
-                    <Td>
-                      <Button
-                        as='a'
-                        size='sm'
-                        fontSize='sm'
-                        colorScheme='green'
-                        leftIcon={<Icon fontSize='16' as={RiPencilLine} />}
-                      >
-                        {isWideVersion ? 'Editar' : ''}
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Gustavo Albunio</Text>
-                    <Text fontSize='sm' color='gray.300'>
-                      gustavo.albunio@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>01 de Junho, 2021</Td>
-                    <Td>
-                      <Button
-                        as='a'
-                        size='sm'
-                        fontSize='sm'
-                        colorScheme='green'
-                        leftIcon={<Icon fontSize='16' as={RiPencilLine} />}
-                      >
-                        {isWideVersion ? 'Editar' : ''}
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Gustavo Albunio</Text>
-                    <Text fontSize='sm' color='gray.300'>
-                      gustavo.albunio@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <>
-                    <Td>01 de Junho, 2021</Td>
-                    <Td>
-                      <Button
-                        as='a'
-                        size='sm'
-                        fontSize='sm'
-                        colorScheme='green'
-                        leftIcon={<Icon fontSize='16' as={RiPencilLine} />}
-                      >
-                        {isWideVersion ? 'Editar' : ''}
-                      </Button>
-                    </Td>
-                  </>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify='center'>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify='center'>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme='whiteAlpha'>
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
+                      <Checkbox colorScheme='pink' />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && (
+                      <>
+                        <Th>Data de cadastro</Th>
+                        <Th width='8'></Th>
+                      </>
+                    )}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.users.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='sm' color='gray.300'>
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && (
+                        <>
+                          <Td>{user.createdAt}</Td>
+                          <Td>
+                            <Button
+                              as='a'
+                              size='sm'
+                              fontSize='sm'
+                              colorScheme='green'
+                              leftIcon={
+                                <Icon fontSize='16' as={RiPencilLine} />
+                              }
+                              onMouseEnter={() => handlePrefetchUser(user.id)}
+                            >
+                              {isWideVersion ? 'Editar' : ''}
+                            </Button>
+                          </Td>
+                        </>
+                      )}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
